0515c304a6ea75830cb788b4757fd745fa07497e
ShortHair32 <1044919284@qq.com>
2019-06-08 12:44:59 +0800

    密码bean

--- src/main/java/com/cet/homework/HomeworkApplication.java
+++ src/main/java/com/cet/homework/HomeworkApplication.java
@@ -3,7 +3,10 @@ package com.cet.homework;
 import com.cet.homework.repository.impl.CustomizedRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = CustomizedRepositoryImpl.class)
@@ -12,5 +15,8 @@ public class HomeworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(HomeworkApplication.class, args);
     }
-
+   @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+   }
 }
