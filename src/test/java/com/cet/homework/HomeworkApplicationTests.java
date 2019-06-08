853694151bf6db505fde8c33d2529bc569f3b7cd
ShortHair32 <1044919284@qq.com>
2019-06-08 13:26:14 +0800

    1

--- src/test/java/com/cet/homework/HomeworkApplicationTests.java
+++ src/test/java/com/cet/homework/HomeworkApplicationTests.java
@@ -1,7 +1,9 @@
 package com.cet.homework;
 
+import ch.qos.logback.classic.Logger;
 import com.cet.homework.entity.User;
 import com.cet.homework.repository.UserRepository;
+import com.cet.homework.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,8 @@ public class HomeworkApplicationTests {
     private EntityManager em;
     @Autowired
     private UserRepository userdao;
+    @Autowired
+    private UserService userservice;
     @Test
     public void contextLoads() {
     }
@@ -26,5 +30,10 @@ public class HomeworkApplicationTests {
         u.setName("blake");
         u.setPassword("123456");
         userdao.save(u);
+    }
+    @Test
+    public void userservicetest(){
+        User u=userservice.getUser("15546503308");
+        System.out.println(u.getName());
     }
 }
