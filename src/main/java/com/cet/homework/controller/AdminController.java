c9f073ee76c3100c69109f1f3433e58c92767691
noone6 <1831779018@qq.com>
2019-06-08 15:09:22 +0800

    白 ExamInfo  controller层增加和更新监考教师请求

--- src/main/java/com/cet/homework/controller/AdminController.java
+++ src/main/java/com/cet/homework/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.cet.homework.controller;
+import com.cet.homework.entity.ExamInfo;
 import com.cet.homework.entity.User;
+import com.cet.homework.service.ExamInfoService;
 import com.cet.homework.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+
+
 //王嘉奇
 @Slf4j
 @RestController
@@ -15,7 +19,7 @@ import java.util.Map;
 public class AdminController {
     @Autowired
     private UserService userservice;
-
+    private ExamInfoService examInfoService;
     /**
      * 添加用户，并返回所有用户信息
      */
@@ -24,4 +28,16 @@ public class AdminController {
         userservice.addUser(user);
         return Map.of("users", userservice.listusers());
     }
+
+    @PostMapping("/ExamInfo")
+    public Map postExamInfo(@RequestBody ExamInfo examInfo,@RequestAttribute int status){
+        examInfoService.addExamInfo(examInfo);
+        return Map.of("examInfo",examInfoService.listexamInfo(status));
+    }
+
+    @PostMapping("/ExamInfo/updateteacher")
+    public void updateTeacher(@RequestBody ExamInfo examInfo){
+        examInfoService.updateExamInfo(examInfo);
+    }
+
 }
--- src/main/java/com/cet/homework/repository/ExamInfoRepository.java
+++ src/main/java/com/cet/homework/repository/ExamInfoRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 
 //白
 //监考信息持久化
@@ -17,7 +19,8 @@ public interface ExamInfoRepository extends CustomizedRepository<ExamInfo, Integ
     /**
      *根据教室查找考试信息
      */
-
+    @Query("SELECT e FROM ExamInfo e WHERE e.status=:status")
+    List<ExamInfo> listexamInfos(@Param("status") int status);
     @Query("SELECT e FROM ExamInfo e WHERE e.classRoom=:classRoom")
     ExamInfo findExamInfo(@Param("classRoom") String classRoom);
 }
