ef38dfb36febd2bae16b2a99f6ad74535840ca70
noone6 <1831779018@qq.com>
2019-06-09 23:23:12 +0800

    注释考试信息细节实体类

---
+++ src/main/java/com/cet/homework/entity/ExamInfoDetail.java
@@ -0,0 +1,26 @@
+package com.cet.homework.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class ExamInfoDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(unique=true)
+    private ExamInfo examInfo;
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+            updatable = false, insertable = false)
+    private LocalDateTime insertTime;
+}
