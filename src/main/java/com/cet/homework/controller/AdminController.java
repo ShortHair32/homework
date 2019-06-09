70b945ced3d793ca981ae33042740c385843203a
noone6 <1831779018@qq.com>
2019-06-09 15:50:31 +0800

    白 ExamInfo  controller层增加和更新监考教师请求

--- src/main/java/com/cet/homework/controller/AdminController.java
+++ src/main/java/com/cet/homework/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.cet.homework.controller;
+import com.cet.homework.entity.ExamInfo;
 import com.cet.homework.entity.User;
+import com.cet.homework.service.ExamInfoService;
 import com.cet.homework.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,9 @@ public class AdminController {
     private UserService userservice;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private ExamInfoService examInfoService;
+
     /**
      * 添加用户，并返回所有用户信息
      */
@@ -26,25 +31,47 @@ public class AdminController {
         userservice.addUser(user);
         return Map.of("users", userservice.listusers());
     }
+
     //注册
     @PostMapping("/register")
-    public void register(@RequestBody User user){
-    user.setPassword(passwordEncoder.encode(user.getPassword()) );
-    userservice.addUser(user);
+    public void register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userservice.addUser(user);
     }
+
     //管理员修改指定用户信息
     //也能改变人员权限
     //service层用了save，即相当于更新
     @PostMapping("/modify")
-    public void modify(@RequestBody User user){
-        user.setPassword(passwordEncoder.encode(user.getPassword()) );
+    public void modify(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userservice.addUser(user);
     }
+
     //删除管理员（是将其权限降级还是删除这个人？）
     //根据手机号找到用户，再根据ID删除用户
     //用户和管理员都能删除
     @PostMapping("/removeadmin")
-    public void removeadmin(@RequestAttribute String phone){
+    public void removeadmin(@RequestAttribute String phone) {
         userservice.removeUser(userservice.getUser(phone).getId());
+    }
+
+    //添加考试信息
+    @PostMapping("/ExamInfo")
+    public Map postExamInfo(@RequestBody ExamInfo examInfo) {
+        examInfoService.addExamInfo(examInfo);
+        return Map.of("examInfo", examInfoService.listexamInfos());
+    }
+
+    //重新分配教师
+    @PostMapping("/ExamInfo/updateteacher")
+    public void updateTeacher(@RequestBody ExamInfo examInfo) {
+        examInfoService.updateExamInfo(examInfo);
+    }
+
+    //查找考试信息
+    @PostMapping("/ExamInfo/findExamInfo")
+    public Map findExamInfo() {
+        return Map.of("ExamInfo", examInfoService.listexamInfos());
     }
 }
